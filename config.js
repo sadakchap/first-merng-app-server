d26f2fad5061e1ed98278be30ea53eccd455140e
sadakchap <aliceprerna@gmail.com>
2020-12-04 16:35:46 +0530

    created registerUser resolver

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 
 const MONGO_URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.e9lgd.mongodb.net/${process.env.MONGO_DB_NAME}`;
+const JWT_AUTH_SECRET=process.env.JWT_AUTH_SECRET
 
 module.exports = {
-    MONGO_URL
+    MONGO_URL,
+    JWT_AUTH_SECRET
 }
--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,7 +1,48 @@
 const User = require("../../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { JWT_AUTH_SECRET } = require("../../config");
+const { UserInputError } = require("apollo-server");
 
 module.exports = {
-  Query: {
-    
+  Mutation: {
+    register: async (_, args, context, info) => {
+      const { registerInput: { email, username, password, confirmPassword } } = args;
+      // validate user data
+      // check for existing user
+      if(password !== confirmPassword){
+        throw new Error("Passwords don't match!");
+      }
+      try {
+        const user = await User.findOne({ username });
+        if(user){
+          throw new UserInputError('Username is taken', {
+            errors: {
+              username: "This username is taken"
+            }
+          });
+        }
+        hashedPassword = await bcrypt.hash(password, 12);
+        const newUser = new User({
+          email,
+          username,
+          password: hashedPassword,
+          createdAt: new Date().toISOString()
+        });
+        const res = await newUser.save();
+        const payload = { userId: res._id, email: res.email, username: res.username }
+        const token = jwt.sign(payload, JWT_AUTH_SECRET, {
+          expiresIn: '1d'
+        });
+        return {
+          ...res._doc,
+          id: res._id,
+          token
+        }
+      } catch (err) {
+        console.log(err); 
+        return err;
+      }
+    }
   },
 };
